2afb5eafcabb28adf52ec4a051d2abb4987de6f8
roquen4145 <roquen4145@gmail.com>
2018-09-04 23:56:12 +0900

    Gallery Chooser

--- src/KHDD/app/src/main/cpp/native-lib.cpp
+++ src/KHDD/app/src/main/cpp/native-lib.cpp
@@ -1,6 +1,9 @@
 #include <jni.h>
 #include <string>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
+using namespace cv;
 extern "C" JNIEXPORT jstring
 
 JNICALL
@@ -9,4 +12,16 @@ Java_com_gmail_roquen4145_khdd_MainActivity_stringFromJNI(
         jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_gmail_roquen4145_khdd_MainActivity_ConvertRGBtoGray(JNIEnv *env, jobject instance,
+                                                             jlong matAddrInput,
+                                                             jlong matAddrResult) {
+
+    // TODO
+    Mat &matInput = *(Mat *)matAddrInput;
+    Mat &matResult = *(Mat *)matAddrResult;
+
+    cvtColor(matInput, matResult, CV_RGBA2GRAY);
 }
--- src/KHDD/app/src/main/java/com/gmail/roquen4145/khdd/MainActivity.java
+++ src/KHDD/app/src/main/java/com/gmail/roquen4145/khdd/MainActivity.java
@@ -1,9 +1,9 @@
 package com.gmail.roquen4145.khdd;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import android.Manifest;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -22,6 +22,7 @@ import android.util.Log;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.opencv.android.BaseLoaderCallback;
@@ -30,7 +31,10 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
@@ -38,30 +42,23 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     private static final int GALLERY_IMAGE_REQUEST = 1;
     public static final int CAMERA_PERMISSIONS_REQUEST = 2;
     public static final int CAMERA_IMAGE_REQUEST = 3;
+    public static final int CROP_FROM_IMAGE = 4;
 
     private static final String TAG = "opencv";
     private CameraBridgeViewBase mOpenCvCameraView;
     private Mat matInput;
     private Mat matResult;
 
+    private Uri mImageCaptureUri;
+    private TextView tv_Command;
+    private ImageView iv_ToRead;
+    private String absolutePath;
+
     public native void ConvertRGBtoGray(long matAddrInput, long matAddrResult);
-=======
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.TextView;
-
-public class MainActivity extends AppCompatActivity {
->>>>>>> parent of f7dee10... OpenCV for Android
-=======
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.TextView;
-
-public class MainActivity extends AppCompatActivity {
->>>>>>> parent of f7dee10... OpenCV for Android
 
     // Used to load the 'native-lib' library on application startup.
     static {
+        System.loadLibrary("opencv_java3");
         System.loadLibrary("native-lib");
     }
 
@@ -70,13 +67,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
                 WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_main);
+
+        tv_Command = (TextView)findViewById(R.id.commandText);
+        iv_ToRead = (ImageView)findViewById(R.id.imgToRead);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             //퍼미션 상태 확인
@@ -118,32 +116,99 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startGalleryChooser(){
-        if (PermissionUtils.requestPermission(this, GALLERY_PERMISSIONS_REQUEST, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-            Intent intent = new Intent();
-            intent.setType("image/*");
-            intent.setAction(Intent.ACTION_GET_CONTENT);
-            startActivityForResult(Intent.createChooser(intent, "Select a photo"),
-                    GALLERY_IMAGE_REQUEST);
-        }
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent , GALLERY_IMAGE_REQUEST);
     };
 
     public void startCamera(){
-        if (PermissionUtils.requestPermission(
-                this,
-                CAMERA_PERMISSIONS_REQUEST,
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.CAMERA)) {
-            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            Uri photoUri = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".provider", getCameraFile());
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
-            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            startActivityForResult(intent, CAMERA_IMAGE_REQUEST);
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA_IMAGE_REQUEST);
+    };
+
+
+    @Override
+    public void onActivityResult(int requestCode,int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if(resultCode != RESULT_OK)
+            return;
+
+        switch(requestCode)
+        {
+            case GALLERY_IMAGE_REQUEST:
+            {
+                mImageCaptureUri = data.getData();
+                Log.d("KHDD",mImageCaptureUri.getPath().toString());
+            }
+            case CAMERA_IMAGE_REQUEST:
+            {
+                Log.d("KHDD","In Camera Request");
+                mImageCaptureUri = data.getData();
+                iv_ToRead.setImageURI(data.getData());
+                Log.d("KHDD",data.getData().toString());
+//                Intent intent = new Intent("com.android.camera.action.CROP");
+//                intent.setDataAndType(mImageCaptureUri,"image/*");
+//                intent.putExtra("outputX",200);
+//                intent.putExtra("outputY",200);
+//                intent.putExtra("aspectX",1);
+//                intent.putExtra("aspectY",1);
+//                intent.putExtra("scale",true);
+//                intent.putExtra("return-data",true);
+//                startActivityForResult(intent,CROP_FROM_IMAGE);
+                break;
+            }
+            case CROP_FROM_IMAGE:
+            {
+                final Bundle extras = data.getExtras();
+
+                String filePath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/KHDD"+System.currentTimeMillis()+".jpg";
+
+                if(extras!=null)
+                {
+                    Bitmap photo = extras.getParcelable("data");
+                    iv_ToRead.setImageBitmap(photo);
+
+                    storeCropImage(photo,filePath);
+                    absolutePath=filePath;
+                    break;
+                }
+
+                File f = new File(mImageCaptureUri.getPath());
+                if(f.exists())
+                {
+                    f.delete();
+                }
+            }
         }
-    };
-    public File getCameraFile() {
-        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        return new File(dir, FILE_NAME);
-    };
+    }
+
+    private void storeCropImage(Bitmap bitmap,String filePath)
+    {
+        String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/KHDD";
+        File directory_KHDD = new File(dirPath);
+
+        if(!directory_KHDD.exists())
+            directory_KHDD.mkdir();
+
+        File copyFile = new File(filePath);
+        BufferedOutputStream out = null;
+
+        try {
+            copyFile.createNewFile();
+            out = new BufferedOutputStream(new FileOutputStream(copyFile));
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+
+            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,Uri.fromFile((copyFile))));
+
+            out.flush();;
+            out.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -161,23 +226,87 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-=======
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
-    }
 
->>>>>>> parent of f7dee10... OpenCV for Android
-=======
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
-    }
 
->>>>>>> parent of f7dee10... OpenCV for Android
+
+
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return null;
+    }
+
+    static final int PERMISSIONS_REQUEST_CODE = 1000;
+    String[] PERMISSIONS  = {"android.permission.CAMERA"};
+
+
+    private boolean hasPermissions(String[] permissions) {
+        int result;
+
+        //스트링 배열에 있는 퍼미션들의 허가 상태 여부 확인
+        for (String perms : permissions){
+
+            result = ContextCompat.checkSelfPermission(this, perms);
+
+            if (result == PackageManager.PERMISSION_DENIED){
+                //허가 안된 퍼미션 발견
+                return false;
+            }
+        }
+
+        //모든 퍼미션이 허가되었음
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch(requestCode){
+            case PERMISSIONS_REQUEST_CODE:
+                if (grantResults.length > 0) {
+                    boolean cameraPermissionAccepted = grantResults[0]
+                            == PackageManager.PERMISSION_GRANTED;
+
+                    if (!cameraPermissionAccepted)
+                        showDialogForPermission("앱을 실행하려면 퍼미션을 허가하셔야합니다.");
+                }
+                break;
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void showDialogForPermission(String msg) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder( MainActivity.this);
+        builder.setTitle("알림");
+        builder.setMessage(msg);
+        builder.setCancelable(false);
+        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id){
+                requestPermissions(PERMISSIONS, PERMISSIONS_REQUEST_CODE);
+            }
+        });
+        builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                finish();
+            }
+        });
+        builder.create().show();
+    }
 }
