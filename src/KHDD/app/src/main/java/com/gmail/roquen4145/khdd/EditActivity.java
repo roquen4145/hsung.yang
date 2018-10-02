78e3463fe6e704a16695a5401d1fc3b463b62c82
roquen4145 <roquen4145@gmail.com>
2018-10-02 15:14:06 +0900

    Activity Addtion

---
+++ src/KHDD/app/src/main/java/com/gmail/roquen4145/khdd/EditActivity.java
@@ -0,0 +1,17 @@
+package com.gmail.roquen4145.khdd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+
+    }
+
+
+}
